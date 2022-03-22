a20adfffabb5cc76b47d2f6d83e7041a48e822e3
ilya-guseynov <ilya.guseynov.self@gmail.com>
2022-03-22 21:48:40 +0300

    new: add mock home page

--- src/components/views/home-view.vue
+++ src/components/views/home-view.vue
@@ -6,7 +6,9 @@ export default {
 
 <template>
   <div>
-    <h1 class="rtu-text-3xl rtu-mb-5">Главнавя</h1>
-    <div>Контент</div>
+    <h1 class="rtu-text-3xl rtu-mb-5">Главная</h1>
+    <div>
+      <p>Проект выполнен Груздевой Валерией Дмитриевной (ИКБО-16-18)</p>
+    </div>
   </div>
 </template>
