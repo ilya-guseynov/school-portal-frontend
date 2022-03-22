39b010eff7b61647d6c672810a5b845f28925c67
ilya-guseynov <ilya.guseynov.self@gmail.com>
2022-03-22 21:45:58 +0300

    new: saves auth user to local storage

--- src/stores/auth-store.js
+++ src/stores/auth-store.js
@@ -1,9 +1,19 @@
 import { defineStore } from 'pinia'
 
+const LOCAL_STORGARE_USER = 'LOCAL_STORGARE_USER'
+
 export const authStore = defineStore('authStore', {
   state: () => {
+    let user = null
+
+    const localStorageUserString = localStorage.getItem(LOCAL_STORGARE_USER)
+
+    if (localStorageUserString) {
+      user = JSON.parse(localStorageUserString)
+    }
+
     return {
-      user: null,
+      user,
     }
   },
 
@@ -27,10 +37,12 @@ export const authStore = defineStore('authStore', {
 
   actions: {
     login(user) {
+      localStorage.setItem(LOCAL_STORGARE_USER, JSON.stringify(user))
       this.user = user
     },
 
     logout() {
+      localStorage.removeItem(LOCAL_STORGARE_USER)
       this.user = null
     },
   },
