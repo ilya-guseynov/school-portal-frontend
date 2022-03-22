2b7b1d32f2ef1fb3305aa31c9d9555ba0172ba26
ilya-guseynov <ilya.guseynov.self@gmail.com>
2022-03-22 22:11:28 +0300

    new: add mock profile page

--- src/components/views/profile-view.vue
+++ src/components/views/profile-view.vue
@@ -1,12 +1,40 @@
 <script>
+import { mapState } from 'pinia'
+import { authStore } from '../../stores/auth-store.js'
+
 export default {
   name: 'ProfileView',
+
+  computed: {
+    ...mapState(authStore, ['isAuthenticated', 'currentUser']),
+  },
+
+  mounted() {
+    if (!this.isAuthenticated) {
+      this.$router.push('/login')
+    }
+  },
 }
 </script>
 
 <template>
   <div>
-    <h1 class="rtu-text-3xl rtu-mb-5">Личный кабинет</h1>
-    <div>Контент</div>
+    <template v-if="isAuthenticated">
+      <h1 class="rtu-text-3xl rtu-mb-5">Личный кабинет</h1>
+      <div>
+        <div class="rtu-flex rtu-items-center rtu-space-x-3">
+          <div>ФИО:</div>
+          <div>{{ currentUser.firstName }} {{ currentUser.middleName }} {{ currentUser.lastName }}</div>
+        </div>
+        <div class="rtu-flex rtu-items-center rtu-space-x-3">
+          <div>Логин:</div>
+          <div>{{ currentUser.login }}</div>
+        </div>
+        <div class="rtu-flex rtu-items-center rtu-space-x-3">
+          <div>Роль:</div>
+          <div>{{ currentUser.userType }}</div>
+        </div>
+      </div>
+    </template>
   </div>
 </template>
